b93773425f4de08f6ba7e63a4a449c86cf2ba316
Kaewmanee <64160234@go.buu.ac.th>
2024-09-22 17:08:57 +0700

    feat: Update dependencies and configure @nestjs/config package

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Ctx, MessagePattern, RmqContext } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
@@ -8,5 +9,23 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+  @MessagePattern('order_created')
+  async handleOrderCreated(data: any,@Ctx() context: RmqContext) {
+   
+    // console.log('Order created', data);
+    // console.log('context', context);
+    // return 'Order created ';
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+    console.log('Order created', data);
+    const instock = true;
+    if (instock) {
+      channel.ack(originalMsg);
+      console.log('Order created', data);
+    } else {
+      channel.ack(originalMsg);
+      console.log('Order created  but not in stock', data);
+    }
   }
 }
